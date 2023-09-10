ace20de075c261ecd7b9252fe874f16199d4bd32
laoTan <18282503557@163.com>
2023-09-10 17:37:08 +0800

    add cahracter

---
+++ Source/actionGame/actionGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "actionGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, actionGame, "actionGame" );
